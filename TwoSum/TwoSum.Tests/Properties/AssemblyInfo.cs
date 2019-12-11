49f4e768792b90276cbbbcf0559ba2fbeec7ed3e
ashishnkr <35804048+ashishnkr@users.noreply.github.com>
2019-12-11 10:23:11 +0530

    Changes and simple problem
    Simple problem of two sum. One of the easiest problems on leetcode solved in C# language

---
+++ TwoSum/TwoSum.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TwoSum.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TwoSum.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6afee9af-2295-457d-a80d-3200c0190993")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
